d1b69004f9b40994e14c6ba92d0de40b2208a4ae
Team <team@fastcode.com.br>
2022-05-18 19:44:52 -0300

    Header component creation

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import styles from './styles.module.scss';
+
+export const Header = () => {
+  const { asPath } = useRouter();
+
+  return (
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <img src="/logo.svg" alt="DevNews!" />
+        <nav>
+          <Link href="/">
+            <a className={asPath === '/' && styles.active}>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a className={asPath === '/posts' && styles.active}>Posts</a>
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
